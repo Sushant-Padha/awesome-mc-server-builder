28629ca8d1baf4f2f896c60c432235bcc5bb1bcf
sushant <sushant.padha@gmail.com>
2021-04-09 09:11:39 +0530

    modified .bat exec

--- awesome-mc-server-builder.bat
+++ awesome-mc-server-builder.bat
@@ -3,10 +3,16 @@
 :: directory of script
 SET directory=%~dp0
 
-echo "HELP -->"
+:: powershell script
+SET psscript=%directory%awesome-mc-server-builder.ps1
 
 echo "       __      _    "
 echo "|___| |__ |   |_)  ."
 echo "|   | |__ |__ |    ."
 
-powershell -c "%directory%\awesome-mc-server-builder.ps1 -?"
+powershell -c "get-help %psscript% -full"
+
+timeout 5
+
+:: basic usage
+powershell -c "%psscript%"
