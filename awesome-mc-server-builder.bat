86ea183aa07f9ff410d225b0057533b80462355e
sushant <sushant.padha@gmail.com>
2021-04-09 09:23:32 +0530

    removed help msg

--- awesome-mc-server-builder.bat
+++ awesome-mc-server-builder.bat
@@ -6,13 +6,7 @@ SET directory=%~dp0
 :: powershell script
 SET psscript=%directory%awesome-mc-server-builder.ps1
 
-echo "       __      _    "
-echo "|___| |__ |   |_)  ."
-echo "|   | |__ |__ |    ."
-
-powershell -c "get-help %psscript% -full"
-
-timeout 5
+timeout 3
 
 :: basic usage
 powershell -c "%psscript%"
